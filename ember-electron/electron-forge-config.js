c8598f3e89e62cd486c1b601e1c483b315aa3a9d
Zhongliang Wang <zwang@rebelworks.io>
2017-11-13 22:28:24 +0800

    [UPDATE] Ember and Electron to the latest versions

---
+++ ember-electron/electron-forge-config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  "make_targets": {
+    "win32": [
+      "squirrel"
+    ],
+    "darwin": [
+      "zip"
+    ],
+    "linux": [
+      "deb",
+      "rpm"
+    ]
+  },
+  "electronPackagerConfig": {
+    "packageManager": "yarn"
+  },
+  "electronWinstallerConfig": {
+    "name": ""
+  },
+  "electronInstallerDebian": {},
+  "electronInstallerRedhat": {},
+  "github_repository": {
+    "owner": "",
+    "name": ""
+  },
+  "windowsStoreConfig": {
+    "packageName": ""
+  }
+};
